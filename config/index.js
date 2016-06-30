e469e957119be9aad8ffb978c1e315b4a5e7e11a
fanky5g <appiahbrobbeybenjamin@rocketmail.com>
2016-06-30 07:21:24 +0000

    setting travis-ci env variables

--- config/index.js
+++ config/index.js
@@ -1,20 +1,23 @@
-
-
 var config = require('./' + (process.env.NODE_ENV || 'development') + '.json');
 
 var fileName = "./secrets.json",
-	secrets;
+    secrets;
 
 try {
-  secrets = require(`${fileName}`);
-}
-catch (err) {
-  secrets = {};
-  console.log("unable to read file '" + fileName + "': ", err);
-  console.log("see config/secrets-sample.json for an example");
+    secrets = require(`${fileName}`);
+} catch (err) {
+    console.log("unable to read file '" + fileName + "': ", err);
+    console.log("see config/secrets-sample.json for an example");
+    console.log("resetting to environment variables");
+    secrets = {
+        "smtp:password": process.env["smtp:password"],
+        "superAdmin:password": process.env["superAdmin:password"],
+        "app:secret": process.env["app:secret"]
+    };
+    console.log(secrets);
 }
 
 module.exports = {
-	config: config,
-	secrets: secrets
+    config: config,
+    secrets: secrets
 };
